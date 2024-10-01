742c0000a79267f86c76a4ba8350435fde34b27b
anne.baranger <annebarang@gmail.com>
2024-10-01 19:45:39 +0200

    small changes in sp filter

--- R/new_filtering.R
+++ R/new_filtering.R
@@ -11,11 +11,12 @@ tar_load(c(FUNDIV_data,
 condi.init=tar_read(climate.cat)$species.cat
 FUNDIV_plotcat=tar_read(climate.cat)$FUNDIV_plotcat
 s_p=species.list.ipm[sp_id]
+out<-setNames(data.frame(matrix(nrow=0,ncol=27)),
+              nm=t)
 for(s_p in species.list.disturbance){
   sp=gsub("_"," ",s_p)
   print(sp)
-  # species.combinations <- 
-  FUNDIV_data |>
+  species.combinations <- FUNDIV_data |>
     # compute basal area ratio of species per plot
     filter(dbh1>0) |>
     group_by(plotcode) |> 
@@ -49,21 +50,23 @@ for(s_p in species.list.disturbance){
     filter(n_species<nsp_per_richness) |>
     # mark wether species combination are simulable
     rowwise() |> 
-    mutate(is.sim=(n_species==sum(sapply(species.list.ipm,function(x)grepl(x,species_combination))))) |> 
+    mutate(is.sim=(n_species==sum(sapply(species.list.ipm,function(x)grepl(x,species_combination)))),
+           is.sp=!(species_combination==s_p)) |> 
     # Compute proportion and cumulated prop
     group_by(clim_id) |>
     arrange(clim_id, desc(n)) |> 
-    mutate(prop=n/sum(n),
+    mutate(prop=(n*is.sp)/sum(n*is.sp), # prop not accounting for species alone
            prop_cum=cumsum(prop),
-           prop_cum_filt=cumsum(prop*is.sim),
-           max_cum_filt=max(prop_cum_filt),
-           prop_filt=(n*is.sim)/sum(n*is.sim),
-           prop_cum_scale=cumsum(prop_filt),
-           is.sp=!(species_combination==s_p),
-           prop_nonsp=(n*is.sp*is.sim)/sum(n*is.sp*is.sim),
-           prop_cum_nonsp=cumsum(prop_nonsp)) |> View()
-    select(clim_id,max_cum_filt) |> unique() |> print()
+           prop_cum_filt=is.sp*cumsum(prop*is.sim),
+           max_cum_filt=max(prop_cum_filt))   
   
+  out_sp<-condi.init |>
+    filter(species==sp) |> 
+    left_join(species.combinations |> 
+                filter((species_combination==s_p|prop_cum_filt<0.8)&is.sim),
+              by="clim_id") |> 
+    arrange(clim_id,n_species)
+  out<-rbind(out,out_sp)
 }
 
 
@@ -72,26 +75,3 @@ for(s_p in species.list.disturbance){
   # arrange(clim_id, desc(n)) |>
   # mutate(pop_cum_filter=cumsum(prop))
 
-
-species.target<- FUNDIV_plotcat |>
-  filter(species==sp) |> 
-  dplyr::select(clim_id) |> # wai_id,sgdd_id
-  distinct() |> 
-  mutate(species_combination=s_p) |> 
-  left_join(species.combinations |>
-              filter(species_combination==s_p),
-            by=c("clim_id","species_combination")) |> #"wai_id","sgdd_id"
-  mutate(prop_cum=NA)
-
-species.combinations.other<-species.combinations |> 
-  filter(species_combination!=s_p) |>
-  mutate(prop_cum=cumsum(prop)) |> 
-  filter(prop_cum<prop_threshold,
-         n>10,
-         prop>0.02) 
-
-species.clim.combi <- condi.init |>
-  filter(species==sp) |>
-  left_join(rbind(species.target,species.combinations.other),
-            by=c("clim_id")) |> #"wai_id","sgdd_id"
-  arrange(clim_id) #wai_id,sgdd_id
