f5aca4022bcb9193b91eb435a1e1c7f65748c2e7
anne.baranger <annebarang@gmail.com>
2024-11-13 16:39:26 +0100

    SEM fit all together scaled

--- R/sem.R
+++ R/sem.R
@@ -5,19 +5,54 @@ library(ggplot2)
 library(lme4)
 library(glmmTMB)
 library(semEff)
+
+
 tar_load(performance)
 species.combination<-tar_read(species.combination) |> 
   dplyr::select(species,clim_id,species_combination,n_species,n,is.sim,is.sp,prop,prop_all)
+
+
+
 mean_pca<-tar_read(climate.cat)$FUNDIV_plotcat %>% 
-  group_by(species,clim_id) %>% 
+  group_by(species) |> 
+  mutate(pca1_opt=mean(pca1)) |> 
+  group_by(species,pca1_opt,clim_id) %>% 
   summarise(pca1=mean(pca1),pca2=mean(pca2)) %>% 
   ungroup() %>% 
   group_by(species) %>% 
-  mutate(pca_sc=scale(pca1))
+  mutate(pca_sc=as.numeric(scale(pca1)),
+         dist_pca1=abs(pca1-pca1_opt)/(max(pca1)-min(pca1))) |> 
+  ungroup()
+
+mean_pca |> ggplot(aes(dist_pca1,species))+geom_boxplot()
 
 performance_mod<-performance |> 
+  # filter(gsub(" ","_",species)!=species_combination) |> 
   left_join(mean_pca, by=c("species","clim_id")) |> 
-  left_join(species.combination,by=c("species","clim_id","species_combination"))
+  left_join(species.combination,by=c("species","clim_id","species_combination")) |>
+  dplyr::select(species,clim_id,species_combination,excluded,competexcluded,metric, metric_val,ba_target,ba_partner,pca1,pca1_opt,dist_pca1,prop_all) |> 
+  filter(metric%in%c("resilience","ba_dif","BA_100","inv_50")) |> 
+  group_by(species,clim_id,species_combination) |> 
+  arrange(species,clim_id,species_combination,metric) |> 
+  mutate(ba_partner=case_when(metric%in%c("BA_100","inv_50")~ba_partner[2],
+                                 TRUE~ba_partner),
+         ba_target=case_when(metric%in%c("BA_100","inv_50")~ba_target[2],
+                              TRUE~ba_target),
+         metric_val=case_when(metric=="resilience"~log(metric_val*ba_target),
+                              TRUE~metric_val)) |> 
+  ungroup() |> 
+  group_by(metric) |> 
+  mutate(metric_val=as.numeric(scale(metric_val))) |>
+  ungroup() |> 
+  pivot_wider(names_from="metric",
+              values_from = "metric_val") |> 
+  mutate(pca1=as.numeric(scale(pca1)),
+         dist_pca1=as.numeric(scale(dist_pca1)),
+         ba_partner=as.numeric(scale(ba_partner)),
+         ba_target=as.numeric(scale(ba_target)),
+         pca1_2=pca1^2) 
+  
+  # filter(species%in%c("Abies alba","Carpinus betulus","Fagus sylvatica","Quercus ilex","Picea abies","Quercus robur"))
 
 ## plots 
 performance_mod |>
@@ -28,6 +63,12 @@ performance_mod |>
   geom_point()+
   geom_smooth(se=FALSE)+
   facet_wrap(~species)
+
+performance_mod |>
+  filter(metric=="ba_dif") |>
+  ggplot(aes(pca1,ba_partner,color=log(prop_all)))+
+  geom_point()+
+  geom_smooth(se=FALSE)
 
 
 performance_mod |> 
@@ -46,10 +87,10 @@ sim_res <- simulateResiduals(mod_compet)
 plot(sim_res)
 
 
-mod_data<-performance |> 
-  filter(metric=="resilience") |> 
-  left_join(species.combination.select,by=c("species","clim_id","species_combination")) |> 
-  mutate(pca12=pca1^2)
+mod_data<-performance_mod |> 
+  filter(metric=="ba_dif") |> 
+  dplyr::select(species,species_combination,metric_val,ba_partner,pca1,prop_all) |> 
+  mutate(pca1_2=pca1^2)
 mod_data <- na.omit(mod_data)
 # submodel for climate and competition
 mod_data |> ggplot(aes(ba_partner,pca1,color=species_combination))+
@@ -67,27 +108,33 @@ confint(mod_resi)
 summary(mod_resi)
 # -- Make model
 mod_sem = list(
-  lmer(ba_partner ~ (1|species)+(1|species:species_combination)+ pca1 + I(pca1^2), 
+  lmer(ba_partner ~ (1|species) + (1|species:species_combination) + pca1 + dist_pca1, 
        weights = prop_all,
-       data=subset(performance_mod,metric=="ba_dif")),
-  # lmer(nih~(1|species)+(1|species:species_combination)+
-  #       pca1,data=mod_data), 
-  lmer(metric_val ~  (1|species)+(1|species:species_combination)+ pca1 + I(pca1^2) + pca1*ba_partner, 
-     data=subset(performance_mod,metric=="ba_dif")))
-
-
+       data=performance_mod),
+  # lmer(inv_50 ~ (1|species) + (1|species:species_combination)+ pca1 + dist_pca1 + ba_partner + ba_target + BA_100,
+  #      data=performance_mod),
+  lmer(inv_50 ~ (1|species) + (1|species:species_combination)+ pca1 + dist_pca1 + ba_partner + ba_target + resilience,
+       data=performance_mod),
+  lmer(resilience ~ (1|species) + (1|species:species_combination) + pca1 + dist_pca1 + ba_partner + ba_target ,
+       data=performance_mod),
+  lmer(ba_target ~ (1|species) + (1|species:species_combination) + pca1 + dist_pca1 + ba_partner,
+       data=performance_mod))
 
 # Extract the effects
-boot_sem = bootEff(mod_sem, R = 5, seed = 13, parallel = "no", ran.eff = "species")
+boot_sem = bootEff(mod_sem, R = 100, seed = 13, parallel = "no", ran.eff = "species")
 
+psem_model <- as.psem(mod_sem)
+summary(psem_model)
 piecewiseSEM:::plot.psem(
   piecewiseSEM::as.psem(mod_sem),
   node_attrs = data.frame(shape = "rectangle", color = "black", fillcolor = "grey"),
   layout = "tree"
 )
-# Output list
+
+
 out = list()
 out$sem = mod_sem
 out$boot = boot_sem
 
-# Return output
+GGally::ggpairs(performance_mod[,c("inv_50","ba_dif","resilience")])
+performance |> ggplot(aes(metric_val))+geom_density()+facet_wrap(~metric,scales="free")
